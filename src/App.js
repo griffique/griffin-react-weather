6d60537ee182a1a96f18ddcf12bdc5adc97fe949
Quinn <74791898+griffique@users.noreply.github.com>
2021-01-22 14:29:38 -0500

    Added container to App file for Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather.js";
 
-function App() {
+import Footer from "./Footer.js";
+
+export default function App() {
+  function change_background() {
+    let day = new Date();
+    let now = day.getHours();
+
+    if (now > 5 && now < 12) {
+      document.body.className = "morning";
+    } else if (now > 12 && now < 18) {
+      document.body.className = "day";
+    } else {
+      document.body.className = "night";
+    }
+  }
+
+  change_background();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Weather />
+      <Footer />
     </div>
   );
 }
-
-export default App;
